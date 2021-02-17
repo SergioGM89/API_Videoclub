1bc73b5fba1d97c3d59e668bc2a37de4c1bbf805
Sergio Gil <serginho_gil@hotmail.com>
2021-02-17 19:46:30 +0100

    programado el get de peliculas

--- app/Controllers/PeliculaController.php
+++ app/Controllers/PeliculaController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Controllers;
 use App\Models\PeliculaModel;
-//use App\Models\EquipoModel;
+use App\Models\ActorModel;
+use App\Models\DirectorModel;
 use CodeIgniter\RESTful\ResourceController;
 
 class PeliculaController extends ResourceController{
@@ -34,87 +35,181 @@ class PeliculaController extends ResourceController{
     }
 
     private function map($data){
-        $peliculas = array();
+        $peliculas = array();       
         foreach($data as $row){
-            $pelicula = array(
-                "id" => $row['id'],
-                "titulo" => $row['titulo'],
-                "anyo" => $row['anyo'],
-                "duracion" => $row['duracion'],
-                "actores" => array(
-                    "id_actor" => $row['id_actor'],
-                    "nombre_actor" => $row['nombre_actor'],
-                    "anyoN_actor" => $row['anyoN_actor'],
-                    "pais_actor" => $row['pais_actor'],
-                    "links_actor" => array(
+            $peli_actualizada = false;
+            if(count($peliculas)>0){//Accedemos al IF si ya hemos guardado los datos de alguna película
+                for($i=0; $i<(count($peliculas)); $i++){
+                    if($peliculas[$i]['id'] == $row['id']){//Y si la película actual coincide con otro, guardamos el nuevo actor
+                        $actor = array(
+                            "id_actor" => $row['id_actor'],
+                            "nombre_actor" => $row['nombre_actor'],
+                            "anyoN_actor" => $row['anyoN_actor'],
+                            "pais_actor" => $row['pais_actor'],
+                            "links_actor" => array(
+                                array(
+                                    "rel" => "actores",
+                                    "href" => $this->url("/ActorController/".$row['id_actor']),
+                                    "action" => "GET",
+                                    "types" => ["text/xml", "application/json"]
+                                ),
+                                array(
+                                    "rel" => "actores",
+                                    "href" => $this->url("/ActorController/".$row['id_actor']),
+                                    "action" => "PUT",
+                                    "types" => ["application/x-www-form-urlencoded"]
+                                ),
+                                array(
+                                    "rel" => "actores",
+                                    "href" => $this->url("/ActorController/".$row['id_actor']),
+                                    "action" => "DELETE",
+                                    "types" => []
+                                )
+                            )//Cierro array de links_actor
+                        );
+                        array_push($peliculas[$i]['actores'], $actor);//Añadimos el actor a la película correspondiente
+                        $peli_actualizada = true;
+                    }
+                }
+            }
+            if($peli_actualizada){
+                continue;
+            }
+            if($row['id_actor'] == null){
+                $pelicula = array(
+                    "id" => $row['id'],
+                    "titulo" => $row['titulo'],
+                    "anyo" => $row['anyo'],
+                    "duracion" => $row['duracion'],
+                    "directores" => array(
+                        "id_director" => $row['id_director'],
+                        "nombre_director" => $row['nombre_director'],
+                        "anyoN_director" => $row['anyoN_director'],
+                        "pais_director" => $row['pais_director'],
+                        "links_director" => array(
+                            array(
+                                "rel" => "directores",
+                                "href" => $this->url("/DirectorController/".$row['id_director']),
+                                "action" => "GET",
+                                "types" => ["text/xml", "application/json"]
+                            ),
+                            array(
+                                "rel" => "directores",
+                                "href" => $this->url("/DirectorController/".$row['id_director']),
+                                "action" => "PUT",
+                                "types" => ["application/x-www-form-urlencoded"]
+                            ),
+                            array(
+                                "rel" => "directores",
+                                "href" => $this->url("/DirectorController/".$row['id_director']),
+                                "action" => "DELETE",
+                                "types" => []
+                            )
+                        )//Cierro array de links_director
+                    ),
+                    "links" => array(
                         array(
-                            "rel" => "actores",
-                            "href" => $this->url("/ActorController/".$row['id_actor']),
+                            "rel" => "self",
+                            "href" => $this->url("/PeliculaController/".$row['id']),
                             "action" => "GET",
                             "types" => ["text/xml", "application/json"]
                         ),
                         array(
-                            "rel" => "actores",
-                            "href" => $this->url("/ActorController/".$row['id_actor']),
+                            "rel" => "self",
+                            "href" => $this->url("/PeliculaController/".$row['id']),
                             "action" => "PUT",
                             "types" => ["application/x-www-form-urlencoded"]
                         ),
                         array(
-                            "rel" => "actores",
-                            "href" => $this->url("/ActorController/".$row['id_actor']),
+                            "rel" => "self",
+                            "href" => $this->url("/PeliculaController/".$row['id']),
                             "action" => "DELETE",
                             "types" => []
                         )
-                    )//Cierro array de links_actor
-                ),
-                "id_director" => array(
-                    "id_director" => $row['id_director'],
-                    "nombre_director" => $row['nombre_director'],
-                    "anyoN_director" => $row['anyoN_director'],
-                    "pais_director" => $row['pais_director'],
-                    "links_director" => array(
+                    )//Cierro array de links              
+                );//Cierro array de $pelicula
+                array_push($peliculas, $pelicula);
+            }else{
+                $pelicula = array(
+                    "id" => $row['id'],
+                    "titulo" => $row['titulo'],
+                    "anyo" => $row['anyo'],
+                    "duracion" => $row['duracion'],
+                    "actores" => array (array(
+                        "id_actor" => $row['id_actor'],
+                        "nombre_actor" => $row['nombre_actor'],
+                        "anyoN_actor" => $row['anyoN_actor'],
+                        "pais_actor" => $row['pais_actor'],
+                        "links_actor" => array(
+                            array(
+                                "rel" => "actores",
+                                "href" => $this->url("/ActorController/".$row['id_actor']),
+                                "action" => "GET",
+                                "types" => ["text/xml", "application/json"]
+                            ),
+                            array(
+                                "rel" => "actores",
+                                "href" => $this->url("/ActorController/".$row['id_actor']),
+                                "action" => "PUT",
+                                "types" => ["application/x-www-form-urlencoded"]
+                            ),
+                            array(
+                                "rel" => "actores",
+                                "href" => $this->url("/ActorController/".$row['id_actor']),
+                                "action" => "DELETE",
+                                "types" => []
+                            )
+                        )//Cierro array de links_actor
+                    ),),
+                    "director" => array(
+                        "id_director" => $row['id_director'],
+                        "nombre_director" => $row['nombre_director'],
+                        "anyoN_director" => $row['anyoN_director'],
+                        "pais_director" => $row['pais_director'],
+                        "links_director" => array(
+                            array(
+                                "rel" => "directores",
+                                "href" => $this->url("/DirectorController/".$row['id_director']),
+                                "action" => "GET",
+                                "types" => ["text/xml", "application/json"]
+                            ),
+                            array(
+                                "rel" => "directores",
+                                "href" => $this->url("/DirectorController/".$row['id_director']),
+                                "action" => "PUT",
+                                "types" => ["application/x-www-form-urlencoded"]
+                            ),
+                            array(
+                                "rel" => "directores",
+                                "href" => $this->url("/DirectorController/".$row['id_director']),
+                                "action" => "DELETE",
+                                "types" => []
+                            )
+                        )//Cierro array de links_director
+                    ),
+                    "links" => array(
                         array(
-                            "rel" => "directores",
-                            "href" => $this->url("/DirectorController/".$row['id']),
+                            "rel" => "self",
+                            "href" => $this->url("/PeliculaController/".$row['id']),
                             "action" => "GET",
                             "types" => ["text/xml", "application/json"]
                         ),
                         array(
-                            "rel" => "directores",
-                            "href" => $this->url("/DirectorController/".$row['id']),
+                            "rel" => "self",
+                            "href" => $this->url("/PeliculaController/".$row['id']),
                             "action" => "PUT",
                             "types" => ["application/x-www-form-urlencoded"]
                         ),
                         array(
-                            "rel" => "directores",
-                            "href" => $this->url("/DirectorController/".$row['id']),
+                            "rel" => "self",
+                            "href" => $this->url("/PeliculaController/".$row['id']),
                             "action" => "DELETE",
                             "types" => []
                         )
-                    )//Cierro array de links_director
-                ),
-                "links" => array(
-                    array(
-                        "rel" => "self",
-                        "href" => $this->url("/PeliculaController/".$row['id']),
-                        "action" => "GET",
-                        "types" => ["text/xml", "application/json"]
-                    ),
-                    array(
-                        "rel" => "self",
-                        "href" => $this->url("/PeliculaController/".$row['id']),
-                        "action" => "PUT",
-                        "types" => ["application/x-www-form-urlencoded"]
-                    ),
-                    array(
-                        "rel" => "self",
-                        "href" => $this->url("/PeliculaController/".$row['id']),
-                        "action" => "DELETE",
-                        "types" => []
-                    )
-                )//Cierro array de links              
-            );//Cierro array de $pelicula
+                    )//Cierro array de links              
+                );//Cierro array de $pelicula
             array_push($peliculas, $pelicula);
+            }
         }
         return $peliculas;
     }
@@ -141,26 +236,41 @@ class PeliculaController extends ResourceController{
     //Tipo POST: crea un nuevo recurso, en este caso una nueva pelicula
     public function create(){
         $peliculas = new PeliculaModel();
-        //$equipos = new EquipoModel();
+        $actores = new ActorModel();
+        $directores = new DirectorModel();
+        $peliculas_act = new PeliculaActorModel();
+        $peliculas_dir = new PeliculaDirectorModel();
 
+        //Validamos la película
         //if($this->validate('peliculas')){
         if(true){
-            if(!$this->request->getPost('ID_equipo')){
-                return $this->genericResponse(null, array("ID_equipo" => "No se ha pasado el id del equipo por parámetro."), 500);
+            //Comprobamos el director
+            if(!$this->request->getPost('id_director')){
+                return $this->genericResponse(null, array("id_director" => "No se ha pasado el id del director por parámetro."), 500);
             }
-            /*if(!$equipos->get($this->request->getPost('ID_equipo'))){
-                return $this->genericResponse(null, array("ID_equipo" => "El equipo no existe."), 500);
-            }*/
+            if(!$directores->get($this->request->getPost('id_director'))){
+                return $this->genericResponse(null, array("id_director" => "El director no existe."), 500);
+            }
+            //Comprobamos los actores
+            if($this->request->getPost('numActores') > 0){
+                for($i=0; $i<($this->request->getPost('numActores')); $i++){
+                    if(!$this->request->getPost([actores][$i])){
+                        return $this->genericResponse(null, array("[actores][$i]" => "No se ha pasado el id del actor por parámetro."), 500);
+                    }
+                    if(!$actores->get($this->request->getPost([actores][$i]))){
+                        return $this->genericResponse(null, array("[actores][$i]" => "El actor no existe."), 500);
+                    }
+                }
+                for($i=0; $i<($this->request->getPost('numActores')); $i++){
+                    $id_actor = $peliculas_act->insert([
+                        'id_pelicula' => $this->request->getPost('titulo'),
+                        'id_actor' => $this->request->getPost([actores][$i])
+                    ]);
+            
             $id = $peliculas->insert([
-                'Nombre' => $this->request->getPost('Nombre'),
-                'ID_equipo' => $this->request->getPost('ID_equipo'),
-                'Anyo_Inicio' => $this->request->getPost('Anyo_Inicio'),
-                'Anyo_Fin' => $this->request->getPost('Anyo_Fin'),
-                'Posicion' => $this->request->getPost('Posicion'),
-                'Altura' => $this->request->getPost('Altura'),
-                'Peso' => $this->request->getPost('Peso'),
-                'Nacimiento' => $this->request->getPost('Nacimiento'),
-                'Procedencia' => $this->request->getPost('Procedencia')
+                'titulo' => $this->request->getPost('titulo'),
+                'anyo' => $this->request->getPost('anyo'),
+                'duracion' => $this->request->getPost('duracion')
             ]);
 
             //Internamente $this->model hace referencia a $modelName = 'App\Models\PeliculaModel';

66ee0515490a28ec2f7813d8f9869f640b71d945
Sergio Gil <serginho_gil@hotmail.com>
2021-02-20 12:10:20 +0100

    Adjunto BBDD Videoclub

---
+++ videoclub.sql
@@ -0,0 +1,275 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 20-02-2021 a las 12:05:20
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `videoclub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actores`
+--
+
+CREATE TABLE `actores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `anyoNacimiento` varchar(4) NOT NULL,
+  `pais` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `actores`
+--
+
+INSERT INTO `actores` (`id`, `nombre`, `anyoNacimiento`, `pais`) VALUES
+(1, 'Antonio', '1325', 'españa'),
+(2, 'Al Pacino', '1940', 'Estados Unidos'),
+(3, 'Robert Duvall', '1931', 'Estados Unidos'),
+(4, 'James Cann', '1940', 'Estados Unidos'),
+(5, 'Diane Keaton', '1946', 'Estados Unidos'),
+(6, 'Robert de Niro', '1943', 'Estados Unidos'),
+(7, 'Kirk Douglas', '1916', 'Estados Unidos'),
+(8, 'Paco', '1992', 'Estonia'),
+(9, 'Adolphe Menjou', '1890', 'Estados Unidos'),
+(11, 'Walter Matthau', '1920', 'Estados Unidos'),
+(12, 'Susan Sarandon', '1946', 'Estados Unidos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `directores`
+--
+
+CREATE TABLE `directores` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `anyoNacimiento` varchar(4) NOT NULL,
+  `pais` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `directores`
+--
+
+INSERT INTO `directores` (`id`, `nombre`, `anyoNacimiento`, `pais`) VALUES
+(1, 'Francis Ford Coppola', '1939', 'Estados Unidos'),
+(3, 'Billy Wilder', '1906', 'Polonia'),
+(6, 'Juan', '1875', 'España'),
+(7, 'Hugo', '1994', 'Brasil');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `anyo` varchar(4) NOT NULL,
+  `duracion` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `peliculas`
+--
+
+INSERT INTO `peliculas` (`id`, `titulo`, `anyo`, `duracion`) VALUES
+(1, 'El Padrino', '1972', 175),
+(2, 'El Padrino 2', '1974', 200),
+(3, 'Senderos de gloria', '1957', 86),
+(4, 'God', '1000', 401),
+(16, 'Foot', '1000', 401),
+(18, 'Campeones', '2018', 260),
+(19, 'Por trece razones', '2018', 260),
+(20, 'jhdgsfjhsdf', '2018', 260),
+(21, 'jhdgsfjhsdffdg', '2018', 260),
+(22, 'jfgsdfgsfg', '1458', 478),
+(23, 'jhdgsfjhsdffdgytuytjh', '2018', 260),
+(25, 'jhdgsfjhsdffdgytuytjhghfh', '2018', 260),
+(26, 'Psicosis', '1992', 140);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas_actores`
+--
+
+CREATE TABLE `peliculas_actores` (
+  `id_pelicula` int(11) NOT NULL,
+  `id_actor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `peliculas_actores`
+--
+
+INSERT INTO `peliculas_actores` (`id_pelicula`, `id_actor`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(2, 3),
+(2, 1),
+(2, 5),
+(4, 8),
+(4, 9),
+(4, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas_directores`
+--
+
+CREATE TABLE `peliculas_directores` (
+  `id_pelicula` int(11) NOT NULL,
+  `id_director` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `peliculas_directores`
+--
+
+INSERT INTO `peliculas_directores` (`id_pelicula`, `id_director`) VALUES
+(1, 1),
+(2, 1),
+(4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `guardaCredenciales` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `password`, `guardaCredenciales`) VALUES
+(1, 'Sergio@gmail.com', 'Sergio1', 0),
+(2, 'Inma@gmail.com', 'Inma2', 0),
+(3, 'Arya@gmail.com', 'Arya3', 0),
+(4, 'Manolo@hotmail.com', 'Manolo4', 0),
+(5, 'Isabel@hotmail.com', 'Isabel5', 0),
+(6, 'Juan@hotmail.com', 'Juan6', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `actores`
+--
+ALTER TABLE `actores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `directores`
+--
+ALTER TABLE `directores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQUE` (`titulo`);
+
+--
+-- Indices de la tabla `peliculas_actores`
+--
+ALTER TABLE `peliculas_actores`
+  ADD KEY `pact_pel_FK` (`id_pelicula`),
+  ADD KEY `pact_act_FK` (`id_actor`);
+
+--
+-- Indices de la tabla `peliculas_directores`
+--
+ALTER TABLE `peliculas_directores`
+  ADD KEY `pdir_dir_FK` (`id_director`),
+  ADD KEY `pdir_pel_FK` (`id_pelicula`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQUE` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `actores`
+--
+ALTER TABLE `actores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `directores`
+--
+ALTER TABLE `directores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `peliculas_actores`
+--
+ALTER TABLE `peliculas_actores`
+  ADD CONSTRAINT `pact_act_FK` FOREIGN KEY (`id_actor`) REFERENCES `actores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pact_pel_FK` FOREIGN KEY (`id_pelicula`) REFERENCES `peliculas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `peliculas_directores`
+--
+ALTER TABLE `peliculas_directores`
+  ADD CONSTRAINT `pdir_dir_FK` FOREIGN KEY (`id_director`) REFERENCES `directores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pdir_pel_FK` FOREIGN KEY (`id_pelicula`) REFERENCES `peliculas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
